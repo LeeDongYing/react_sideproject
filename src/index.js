578fe46b79007a146efd829db7c0b6fefd99834c
LeeDongYing <107396912+LeeDongYing@users.noreply.github.com>
2023-03-03 17:05:43 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./pages/Home";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Home />
+  </StrictMode>
+);
